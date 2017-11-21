d743825f6b0e17f61e28ce0f87886c1b2c4565fc
Crista Cooper <aine3d@gmail.com>
2017-11-21 16:44:50 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  index = 0
-  lowest = []
+  
   hash.each do |name, value|
+    lowest = {}
+    index = 0
     if value[index] < value[index + 1]
       lowest = value
       index = index + 1
