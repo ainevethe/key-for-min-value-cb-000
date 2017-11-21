4527abc9ee21600c8ae785c16ac76d34947aaaa9
Crista Cooper <aine3d@gmail.com>
2017-11-21 17:02:12 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,13 @@
 
 def key_for_min_value(hash)
 
-  low_key = nil
+  low_name = nil
   low_val = nil
 
   hash.each do |name, value|
     if low_val == nil || value < low_val
       low_val = value
-      low_key = key
+      low_key = name
     end
   end
   return low_key
